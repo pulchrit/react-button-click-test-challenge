d94bda810045a792b4990c2eb6c196fe929fe1c8
Melissa Lafranchise <pulchrit@gmail.com>
2019-06-24 14:10:41 -0700

    Completed test to assert that prop.onClick was called when the button was clicked

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-
 import Button from './button';
 
 describe('<Button />', () => {
+    const onClick = jest.fn();
+
     it('Should call props.onClick when the button is clicked', () => {
-        const wrapper = shallow(<Button />);
+        const wrapper = shallow(<Button onClick={onClick}/>);
         wrapper.find('button').simulate('click');
+        expect(onClick).toHaveBeenCalled();
     });
 });
