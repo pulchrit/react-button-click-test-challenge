5b6e7f24fdf9d92c930e990d5aab68df20fdc139
Melissa Lafranchise <pulchrit@gmail.com>
2019-06-24 14:17:17 -0700

    added comments to explain where my additions were.

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -3,11 +3,15 @@ import {shallow} from 'enzyme';
 import Button from './button';
 
 describe('<Button />', () => {
-    const onClick = jest.fn();
+    // I added the line below to create a mock function which is required for 
+    // using toHaveBeenCalled().
+    const onClick = jest.fn(); 
 
     it('Should call props.onClick when the button is clicked', () => {
         const wrapper = shallow(<Button onClick={onClick}/>);
         wrapper.find('button').simulate('click');
+        // I added the line below to asser that the mock function by the 
+        // name of onClick is called. 
         expect(onClick).toHaveBeenCalled();
     });
 });
